8464bef6823721480f4219f956ce23178157e8ec
Seb-Prod <sebastien.drillaud@gmail.com>
2024-11-19 15:39:45 +0100

    Ajout filtre dans la récupération des fichiers

--- liste_fichier.py
+++ liste_fichier.py
@@ -1,4 +1,16 @@
 import os
 
 def lister_fichiers(repertoire):
-        return os.listdir(repertoire)
+        #initialisation du tableau qui stockera les noms des images
+        result =[]
+
+        #Tableau d'extensions des fichiers images
+        extension = [".png", ".jpeg", ".jpg", ".webp"]
+
+        #Boucle qui parcour le répertoire et ajoute dans le tableau result uniquement les fichiers images
+        for file in os.listdir(repertoire):
+                if file.endswith(tuple(extension)):
+                        result.append(file)
+
+        #Retourne le tableau        
+        return result
